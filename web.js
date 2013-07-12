54a2b5ef1718308e7f5801291d42f50c53b2c930
rob eddy <robeddy@gmail.com>
2013-07-12 02:37:13 +0000

    Changed encoding value to ascii.

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html','hello node'));
+    response.send(fs.readFileSync('index.html','ascii'));
     //response.send("Hi.");
 });
 
